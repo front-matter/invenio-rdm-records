3666a9f120e6af11bb02cf169254b80385c4ee9f
Martin Fenner <martin@front-matter.io>
2025-08-27 12:24:35 +0200

    fix crossref encoder issues

--- invenio_rdm_records/resources/serializers/crossref/__init__.py
+++ invenio_rdm_records/resources/serializers/crossref/__init__.py
@@ -12,7 +12,7 @@ from flask_resources import BaseListSchema, MarshmallowSerializer
 from flask_resources.serializers import SimpleSerializer
 
 from ....contrib.journal.processors import JournalCrossrefDumper
-from commonmeta import unparse_xml, CrossrefXMLSchema
+from commonmeta import CrossrefXMLSchema
 
 
 class CrossrefXMLSerializer(MarshmallowSerializer):
@@ -20,11 +20,9 @@ class CrossrefXMLSerializer(MarshmallowSerializer):
 
     def __init__(self, **options):
         """Constructor."""
-        encoder = options.get("encoder", unparse_xml)
         super().__init__(
             format_serializer_cls=SimpleSerializer,
             object_schema_cls=CrossrefXMLSchema,
             list_schema_cls=BaseListSchema,
             schema_kwargs={"dumpers": [JournalCrossrefDumper()]},  # Order matters
-            encoder=encoder,
         )
